19293a628393d1b014e4cb7d89a85f11eb8608ec
Ankita Durgavajula <35971200+ad474@users.noreply.github.com>
2020-07-21 01:28:40 +0530

    update routing

--- ReactNativeFrontend/Routing.js
+++ ReactNativeFrontend/Routing.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons'; 
+
 import Language from './pages/Language';
 import Login from './pages/Login';
 import FileFIR from './pages/FileFIR';
@@ -35,6 +37,31 @@ const TabNavigator = createBottomTabNavigator({
     TrackStatus: TrackStatus,
     Profile: Profile,
     MedicalHelp: MedicalHelp
-  });
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        let iconSize;
+        if (routeName === 'FileFIR') {
+          iconName = focused? 'ios-information-circle': 'ios-information-circle-outline';
+          iconSize = focused? 28: 20;
+        } else if (routeName === 'TrackStatus') {
+          iconName = focused ? 'ios-list-box' : 'ios-list';
+          iconSize = focused? 28: 20;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={iconSize} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#ED5A67',
+      inactiveTintColor: '#16335C',
+    },
+  }
+);
 
 export default createAppContainer(TabNavigator);
