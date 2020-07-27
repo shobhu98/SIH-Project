c78a0516637828803f81000ef420f08c6ce6eb74
Dev Kathuria <dk382@snu.edu.in>
2020-07-27 15:04:53 +0530

    login

--- BackEnd/routes/api/admin_auth.js
+++ BackEnd/routes/api/admin_auth.js
@@ -24,6 +24,7 @@ router.post('/',[
 ],async function (req,res) {
     const errors=validationResult(req);
     if(!errors.isEmpty()){
+        console.log(req.body)
         return res.status(400).json({errors:errors.array()});
     }
     const {email,password}=req.body;
