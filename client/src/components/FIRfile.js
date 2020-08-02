a19659561eb6d1b12b35cf156f5e9f5b30cc22b6
Dev Kathuria <dk382@snu.edu.in>
2020-08-02 12:02:28 +0530

    added conditions for closed inv

--- client/src/components/FIRfile.js
+++ client/src/components/FIRfile.js
@@ -3,7 +3,27 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
-import {DropzoneArea,} from 'material-ui-dropzone'
+import { DropzoneArea } from "material-ui-dropzone";
+import MaterialTable from "material-table";
+
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+
 import {
   Divider,
   Button,
@@ -19,6 +39,35 @@ import FIRModalOngoing from "./FIRModalOngoing";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
+import { forwardRef } from "react";
+
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
 
 export default class FIRfile extends Component {
   constructor(props) {
@@ -31,7 +80,33 @@ export default class FIRfile extends Component {
     caseNotes: true,
     status: null,
     files: [],
+
+    columns: [
+      
+      { title: "Name", field: "name" },
+    ],
+
+    data: [{name:"evidence1"}],
+    actions: [
+      (rowData) => ({
+        icon: () => <CloudDownloadIcon />,
+        tooltip: "Download",
+        onClick: (rowData) => this.download(rowData),
+      }),
+      (rowData) => ({
+        icon: () => <DeleteIcon />,
+        tooltip: "Delete",
+        onClick: (rowData) => this.delete(rowData),
+        disabled: this.state.status==="closed"?true:false
+      }),
+    ],
+    open: false,
+    firid: null,
+    status: null
   };
+
+  download(rowData) {}
+  delete(rowData) {}
 
   handleChange(files) {
     this.setState({
@@ -185,6 +260,7 @@ export default class FIRfile extends Component {
                       style={{ height: "100%", width: "100%" }}
                       variant="outlined"
                       onClick={this.setStatus}
+                      disabled= {this.state.status==="closed"?true:false}
                     >
                       Set Status
                     </Button>
@@ -210,48 +286,50 @@ export default class FIRfile extends Component {
                     <Typography variant="h4">Assets</Typography>
                     <Grid container spacing={1}>
                       <Grid item xs={12} className="list">
-                        <ul>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                          <li>File Download</li>
-                        </ul>
+                        <MaterialTable
+                          options={{
+                            actionsColumnIndex: -1,
+                          }}
+                          doubleHorizontalScroll={true}
+                          
+                          icons={tableIcons}
+                          title="Files"
+                          columns={this.state.columns}
+                          data={this.state.data}
+                          actions={this.state.actions}
+                        />
                       </Grid>
 
-
-                      <Grid item xs={12}>
+                      {this.state.status!="closed"?<>
+                      <Grid item xs={12} >
                         <DropzoneArea
                           className="zone"
-                          acceptedFiles={[".csv", ".xlsx",".png",".jpg",".jpeg"]}
+                          acceptedFiles={[
+                            ".csv",
+                            ".xlsx",
+                            ".png",
+                            ".jpg",
+                            ".jpeg",
+                          ]}
                           minSize={0}
                           maxFileSize={10242880}
                           onChange={this.handleChange.bind(this)}
                           filesLimit={1}
                           showFileNames={true}
-                          style={{height:100}}
+                          style={{ height: 100 }}
                         />
                       </Grid>
                       <Grid item xs={12}>
-                        <Button variant="outlined" style={{width:'100%'}} onClick={this.upload.bind(this)}>Upload</Button>
+                        <Button
+                          variant="outlined"
+                          style={{ width: "100%" }}
+                          onClick={this.upload.bind(this)}
+                          
+                        >
+                          Upload
+                        </Button>
                       </Grid>
+                      </>:null}
                     </Grid>
                   </Box>
                 </Paper>
@@ -264,9 +342,56 @@ export default class FIRfile extends Component {
               {this.state.caseAssets === true ? (
                 <Paper>
                   <Box m={2} p={3}>
-                    <Typography variant="h4">Assets</Typography>
+                  <Typography variant="h4">Assets</Typography>
+                    <Grid container spacing={1}>
+                      <Grid item xs={12} className="list">
+                        <MaterialTable
+                          options={{
+                            actionsColumnIndex: -1,
+                          }}
+                          doubleHorizontalScroll={true}
+                          
+                          icons={tableIcons}
+                          title="Files"
+                          columns={this.state.columns}
+                          data={this.state.data}
+                          actions={this.state.actions}
+                        />
+                      </Grid>
+                      {this.state.status!="closed"?<>
+                      <Grid item xs={12} >
+                        <DropzoneArea
+                          className="zone"
+                          acceptedFiles={[
+                            ".csv",
+                            ".xlsx",
+                            ".png",
+                            ".jpg",
+                            ".jpeg",
+                          ]}
+                          minSize={0}
+                          maxFileSize={10242880}
+                          onChange={this.handleChange.bind(this)}
+                          filesLimit={1}
+                          showFileNames={true}
+                          style={{ height: 100 }}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Button
+                          variant="outlined"
+                          style={{ width: "100%" }}
+                          onClick={this.upload.bind(this)}
+                          
+                        >
+                          Upload
+                        </Button>
+                      </Grid>
+                      </>:null}
+                      
+                    </Grid>
 
-                    <Button type="file"></Button>
+                    
                   </Box>
                 </Paper>
               ) : (
@@ -278,7 +403,7 @@ export default class FIRfile extends Component {
           {this.state.caseAssets === true ? (
             <Grid item xs={12} md={6}>
               {this.state.caseNotes === true ? (
-                <Paper> 
+                <Paper>
                   <Box m={2} p={3}>
                     <Typography variant="h4">Notes</Typography>
                     <TextareaAutosize
@@ -288,7 +413,7 @@ export default class FIRfile extends Component {
                       fullWidth={true}
                       style={{ width: "100%" }}
                     ></TextareaAutosize>
-                    <Button variant="outlined" style={{ width: "100%" }}>
+                    <Button variant="outlined" style={{ width: "100%" }} disabled= {this.state.status==="closed"?true:false}>
                       Save
                     </Button>
                   </Box>
@@ -309,7 +434,7 @@ export default class FIRfile extends Component {
                       fullWidth={true}
                       style={{ width: "100%" }}
                     ></TextareaAutosize>
-                    <Button variant="outlined" style={{ width: "100%" }}>
+                    <Button variant="outlined" style={{ width: "100%" }} disabled= {this.state.status==="closed"?true:false}>
                       Save
                     </Button>
                   </Box>
